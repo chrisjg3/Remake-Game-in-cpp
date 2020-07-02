4986735c10426e603f3001ab53e98f54668d27f9
Chris Joseph Gallo <chrisjg3@users.noreply.github.com>
2020-07-01 22:50:51 -0400

    Added empty file and some ideas

---
+++ basefile.cpp
@@ -0,0 +1,19 @@
+/*
+base game ideas:
+
+
+Haven't typed much code until I can think of some ideas.  
+I want to change it a bit from the Python type game, maybe add some random generation.
+*/
+
+#include <iostream>
+
+
+int main()
+{
+
+
+
+    return 0;
+}
+
