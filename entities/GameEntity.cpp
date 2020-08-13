bd808e19c9f5c2cffb58df2570810249530cb012
Christopher Gallo <cjg100@georgetown.edu>
2020-08-13 01:38:59 -0400

    Started GameEntity attributes and methods

--- entities/GameEntity.cpp
+++ entities/GameEntity.cpp
@@ -1,3 +1,44 @@
 
 #include "GameEntity.hpp"
 
+Entity::Entity(unsigned short health, unsigned short healthMax, bool isPlayer)
+{
+    this->gameStats[0] = health;
+    this->gameStats[1] = 0;
+    this->gameStats[2] = healthMax;
+
+    if(isPlayer)
+    {
+        // This section builds the skills if this entity is the player character
+        // Currently use (in order): Strength, Persuasion, Repairing, Animal-Handling
+        std::cout << "\nPlease input skills...(Currently not implemented)!!!\n";
+        skills[0] = 3; // Strength
+        skills[1] = 4; // Persuasion
+        skills[2] = 5; // Repair
+        skills[3] = 6; // Animal-Handling
+    }
+}
+
+
+// Inventory related methods:
+void Entity::pickUpItem(Item&)
+{
+    if((!inventory.size()) < 30) { std::cout<<"\nInventory is full! \n"; return; }
+
+
+}
+
+
+
+void Entity::dropItem(Item&)
+{
+    
+}
+
+
+
+
+bool Entity::findItem(Item&)
+{
+    
+}
--- entities/GameEntity.hpp
+++ entities/GameEntity.hpp
@@ -2,6 +2,7 @@
 #define GAMEENTITY_HPP
 
 #include <vector>
+#include <iostream>
 
 #include "../items/Item.hpp"
 
@@ -9,11 +10,31 @@
 class Entity
 {
 private:
-std::vector <Item*> inventory;
-int gameStats[3];
+std::vector <Item*> inventory; // Max will be 30, figuring out based way to implement this
+
+// Three GameStats are: Health, Damage Resitance (effected by armor/items), max health (keeps track of upper bound)
+unsigned short gameStats[3]; 
+
+// Skills effect how effective things are in-game.  Currently use (in order): Strength, Persuasion, Repairing, Animal-Handling
+unsigned short skills[4]; // Override Operator to make [] work with strings for above skills? might be easier !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+public:
+// Default Constructor
+Entity();
+
+// Main Constructor used in-game
+Entity(unsigned short health, unsigned short healthMax, bool isPlayer);
+
+// Copy constuctor?  How to make many enemies easy to make
 
 
+// inventory related functions
+void pickUpItem(Item&);
 
+void dropItem(Item&);
+
+// How do we compare items????!!! Gotta figuer out the best way to do this, maybe overload relational operators!!!!!!!!!!!!!!!!!!!!!!
+bool findItem(Item&); // Used when need to find exact item, returns true if found and used.
 
 
 };
