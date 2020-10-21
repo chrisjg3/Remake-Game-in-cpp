32e79adbf9e39aa660ff0274719d1d422a9710bb
Christopher Gallo <cjg100@georgetown.edu>
2020-10-20 23:23:39 -0400

    File Restructure: - Finally learned Rust module system, so names and directories updated - mod.rs added for entity_structs - inventory made public - some test methods were made, but these will likely be changed a lot in the coming days as I actually make the game, so won't note any

---
+++ src/entity_structs/mod.rs
@@ -0,0 +1,2 @@
+pub mod inventory;
+mod node;
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 // use std::io;
 mod gamestruct;
+mod entity_structs;
+
+use entity_structs::inventory;
+
 
 fn main() {
-    // let game: gamestruct::GameStr;
 
-    loop 
+    loop
     {
         let (direct, times_move) = gamestruct::GameStr::read_player();
         gamestruct::GameStr::check_events(direct, times_move);
         gamestruct::GameStr::update_interface();
     }
     
+    // inventory::Entity::new();
+
 }
