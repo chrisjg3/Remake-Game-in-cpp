598ed3363509afad13700d3ee66769d1e0723281
chris <chris.j.gallo3@gmail.com>
2021-01-12 13:09:50 -0500

    starting restructure, and beginning js front end first

---
+++ backend_rs/target/debug/deps/cfg_if-b10e0993eefc366b.d
@@ -0,0 +1,7 @@
+/home/chris/github/game/backend_rs/target/debug/deps/cfg_if-b10e0993eefc366b.rmeta: /home/chris/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-0.1.10/src/lib.rs
+
+/home/chris/github/game/backend_rs/target/debug/deps/libcfg_if-b10e0993eefc366b.rlib: /home/chris/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-0.1.10/src/lib.rs
+
+/home/chris/github/game/backend_rs/target/debug/deps/cfg_if-b10e0993eefc366b.d: /home/chris/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-0.1.10/src/lib.rs
+
+/home/chris/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-0.1.10/src/lib.rs:
---
+++ backend_rs/target/debug/deps/ppv_lite86-9e2dc93c7288ddfd.d
@@ -0,0 +1,11 @@
+/home/chris/github/game/backend_rs/target/debug/deps/ppv_lite86-9e2dc93c7288ddfd.rmeta: /home/chris/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/lib.rs /home/chris/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/soft.rs /home/chris/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/types.rs /home/chris/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/x86_64/mod.rs /home/chris/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/x86_64/sse2.rs
+
+/home/chris/github/game/backend_rs/target/debug/deps/libppv_lite86-9e2dc93c7288ddfd.rlib: /home/chris/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/lib.rs /home/chris/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/soft.rs /home/chris/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/types.rs /home/chris/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/x86_64/mod.rs /home/chris/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/x86_64/sse2.rs
+
+/home/chris/github/game/backend_rs/target/debug/deps/ppv_lite86-9e2dc93c7288ddfd.d: /home/chris/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/lib.rs /home/chris/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/soft.rs /home/chris/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/types.rs /home/chris/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/x86_64/mod.rs /home/chris/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/x86_64/sse2.rs
+
+/home/chris/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/lib.rs:
+/home/chris/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/soft.rs:
+/home/chris/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/types.rs:
+/home/chris/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/x86_64/mod.rs:
+/home/chris/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.10/src/x86_64/sse2.rs:
