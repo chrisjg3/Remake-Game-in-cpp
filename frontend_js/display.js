44be48a6c190399297bdf2ad9f59f791a1e91db1
chris <chris.j.gallo3@gmail.com>
2021-01-12 15:46:22 -0500

    Finished very basic display and restruct.

--- frontend_js/display.js
+++ frontend_js/display.js
@@ -1,36 +1,54 @@
 const canvas = document.getElementById("canvas");
 const context = document.getElementById("canvas").getContext("2d");
 
-function drawPlanet(x, y, color, size, correction)
+function drawHex(ctx, size, Xcenter, Ycenter, color) // code for specific hex drawing
 {
-// draw orbit
-context.beginPath();
-context.moveTo(1800, 1200);
-context.arc(1800,1200,2000-Math.sqrt(x*x+y*y)+correction,0,2*Math.PI); // poorly done math, but now it stays
-context.stroke();
-// actual planet
-context.beginPath();
-context.moveTo(x, y);
-context.arc(x,y, size,0,2*Math.PI);
-context.stroke();
-context.fillStyle = color;
-context.fill();
+    const numberOfSides = 6;
+
+    ctx.beginPath();
+    ctx.moveTo (Xcenter +  size * Math.cos(0), Ycenter +  size *  Math.sin(0)); 
+            
+    for (var i = 1; i <= numberOfSides;i += 1) {
+    ctx.lineTo (Xcenter + size * Math.cos(i * 2 * Math.PI / numberOfSides), Ycenter + size * Math.sin(i * 2 * Math.PI / numberOfSides));
+    }
+
+    ctx.strokeStyle = "#000000";
+    ctx.lineWidth = 1;
+    ctx.stroke();
+    ctx.fillStyle = color;
+    ctx.fill();
+}
+
+let ownership_arry = ['silver', 'silver', 'silver', 'silver', 'silver', 'silver', 'silver'];
+
+function redrawBoard(context, ownArr) // code for re-rendering the board
+{
+    // top layer
+    drawHex(context, 20, 256, 207, ownArr[0]);
+
+    // next layer
+    drawHex(context, 20, 225, 225, ownArr[1]);
+    drawHex(context, 20, 288, 226, ownArr[2]);
+
+    // next
+    drawHex(context, 20, 256, 244, ownArr[3]);
+
+    // next
+    drawHex(context, 20, 287, 263, ownArr[4]);
+    drawHex(context, 20, 224, 262, ownArr[5]);
+
+    // last layer
+    drawHex(context, 20, 256, 281, ownArr[6]);
 
 }
 
+let done = false;
+redrawBoard(context, ownership_arry);
 
-// Sun **
-context.beginPath();
-context.moveTo(1800, 1200);
-context.arc(1800,1200,1000,0,2*Math.PI);
-context.stroke();
-context.fillStyle = "orange"
-context.fill();
-// ** //
+ownership_arry[0] = "red";
 
-// Planets **
-drawPlanet(400, 400, "red", 20, 180); // planet a
-drawPlanet(200, 200, "blue", 20, 170); // planet b
-// ** //
-
-// drawPlanet(100, 100, "green", 30, 160) // test and hypotheical extra planet
+while(!done)
+{
+    prompt("Did they win yet?");
+    redrawBoard(context, ownership_arry);
+}
