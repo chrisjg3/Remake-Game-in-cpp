8fe2a82da5ef893271a99f26d3c55bf43b79befa
chris <chris.j.gallo3@gmail.com>
2021-02-14 18:02:29 -0500

    ran cargo clean to remove some Makefile.

--- modernia_backend/target/debug/deps/modernia_backend-c0e62aa191cf4b6d.d
+++
@@ -1,5 +0,0 @@
-/home/chris/github/risk_game/modernia_backend/target/debug/deps/modernia_backend-c0e62aa191cf4b6d.rmeta: src/main.rs
-
-/home/chris/github/risk_game/modernia_backend/target/debug/deps/modernia_backend-c0e62aa191cf4b6d.d: src/main.rs
-
-src/main.rs:
--- modernia_backend/target/debug/modernia_backend.d
+++
@@ -1,1 +0,0 @@
-/home/chris/github/risk_game/modernia_backend/target/debug/modernia_backend: /home/chris/github/risk_game/modernia_backend/src/lib.rs /home/chris/github/risk_game/modernia_backend/src/main.rs
