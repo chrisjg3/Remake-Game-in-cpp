67713af1aafbfe6190cc996b390befbfd66fa5d0
chris <chris.j.gallo3@gmail.com>
2021-02-14 19:46:16 -0500

    added some rnaodm number generation and thread sleeping to rust backend, to test.

--- modernia_backend/src/main.rs
+++ modernia_backend/src/main.rs
@@ -6,17 +6,31 @@ use std::io::Write;
 use std::time::Duration;
 use std::thread;
 
+// random number gernation
+use rand::Rng;
+
 fn main() {
     
     let listener = TcpListener::bind("127.0.0.1:50000").expect("Not able to connect to port!");
 
     let (mut socket, _addr) = listener.accept().unwrap();
 
+    let mut rng = rand::thread_rng();
 
-    
     // thread::sleep(Duration::from_millis(1000));
-    println!("about to send 9");
-    socket.write(&[9, 34, 52, 18]).unwrap();
+    println!("generating random u8");
+
+    let n1: u8 = rng.gen_range(0..20);
+
+
+    thread::sleep(Duration::from_millis(1000));
+
+    println!("generating second random u8");
+
+    let n2: u8 = rng.gen_range(11..31);
+
+
+    socket.write(&[n1, n2]).unwrap();
 
 
     println!("Complete");
